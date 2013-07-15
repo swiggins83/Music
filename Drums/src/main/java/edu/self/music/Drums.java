5a76b63254a55c19e947039d36a1cffa39f4799d
swiggins83 <stevenwiggins83@gmail.com>
2013-07-15 13:56:00 -0400

    Idioteque?

--- Drums/src/main/java/edu/self/music/Drums.java
+++ Drums/src/main/java/edu/self/music/Drums.java
@@ -14,23 +14,60 @@ public class Drums {
     public static void main(String[] args) {
 
         Player player = new Player();
-		Rhythm rhythm = new Rhythm();
+		Rhythm rhythm1 = new Rhythm();
+		Rhythm rhythm1Var = new Rhythm();
+		Rhythm rhythm1Long = new Rhythm();
+
+		rhythm1.setLayer(1, "o.........o...o.o....o..o.......");
+		rhythm1.setLayer(2, "....*.......*.......*.......*...");
+		rhythm1.setLayer(3, "................................");
+		rhythm1.setLayer(4, "................................");
+
+		rhythm1Var.setLayer(1, "..o...o.o....o..o.......");
+		rhythm1Var.setLayer(2, "....*.......*.......*...");
+		rhythm1Var.setLayer(3, "........................");
+		rhythm1Var.setLayer(4, "........................");
+
 		
-		rhythm.setLayer(1, "o...o...o..ooo..");
-		rhythm.setLayer(2, "..*...*...*...*.");
-		rhythm.setLayer(3, "^^^^^^^^^^^^^^^^");
-		rhythm.setLayer(4, "!...............");
+		rhythm1Long.setLayer(1, "o.........o...o.o....o..o...............................");
+		rhythm1Long.setLayer(2, "....*.......*.......*.......*.......*.......*.......*...");
+		rhythm1Long.setLayer(3, "........................................................");
+		rhythm1Long.setLayer(4, "!.......................................................");
 
-		rhythm.addSubstitution('o', "[BASS_DRUM]s");
-		rhythm.addSubstitution('*', "[ACOUSTIC_SNARE]s");
-		rhythm.addSubstitution('^', "[PEDAL_HI_HAT]s");
-		rhythm.addSubstitution('!', "[CRASH_CYMBAL_1]s");
-		rhythm.addSubstitution('.', "Rs");
+		rhythm1.addSubstitution('o', "[BASS_DRUM]s");
+		rhythm1.addSubstitution('*', "[ACOUSTIC_SNARE]s");
+		rhythm1.addSubstitution('^', "[PEDAL_HI_HAT]s");
+		rhythm1.addSubstitution('!', "[CRASH_CYMBAL_1]s");
+		rhythm1.addSubstitution('.', "Rs");
 
-        Pattern pattern = rhythm.getPattern();
-		pattern.repeat(4);
+		rhythm1Var.addSubstitution('o', "[BASS_DRUM]s");
+		rhythm1Var.addSubstitution('*', "[ACOUSTIC_SNARE]s");
+		rhythm1Var.addSubstitution('^', "[PEDAL_HI_HAT]s");
+		rhythm1Var.addSubstitution('!', "[CRASH_CYMBAL_1]s");
+		rhythm1Var.addSubstitution('.', "Rs");
 
-        player.play(pattern);
+		rhythm1Long.addSubstitution('o', "[BASS_DRUM]s");
+		rhythm1Long.addSubstitution('*', "[ACOUSTIC_SNARE]s");
+		rhythm1Long.addSubstitution('^', "[PEDAL_HI_HAT]s");
+		rhythm1Long.addSubstitution('!', "[CRASH_CYMBAL_1]s");
+		rhythm1Long.addSubstitution('.', "Rs");
+
+        Pattern rhythm1Pat = rhythm1.getPattern();
+        Pattern rhythm1VarPat = rhythm1Var.getPattern();
+        Pattern rhythm1LongPat = rhythm1Long.getPattern();
+
+		Pattern wholeSong = new Pattern();
+
+		wholeSong.add(rhythm1Pat);
+		wholeSong.add(rhythm1VarPat);
+		wholeSong.add(rhythm1VarPat);
+		wholeSong.add(rhythm1VarPat);
+		wholeSong.add(rhythm1LongPat);
+		wholeSong.add(rhythm1LongPat);
+		wholeSong.add(rhythm1LongPat);
+		wholeSong.add(rhythm1LongPat);
+
+        player.play(wholeSong);
         player.close();
     }
 }
